7327754fae34116ecfe461b06704dd3dcbc6c23f
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-04 23:13:07 +0530

    Implement basic shell structure with command loop and placeholder functions

--- src/main.c
+++ src/main.c
@@ -1,8 +1,42 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+void run_shell();
+void read_line();
+bool execute_command();
 
 int main(int argc, char *argv[])
 {
-    printf("Hello World!");
+    // Setup: Initialize the shell and any configuration
+
+    // Loop: Read commands from stdin and executes them
+    run_shell();
+
+    // Teardown: Shutdown cleanup logic
+
     return EXIT_SUCCESS;
+}
+
+void run_shell()
+{
+    bool run = true;
+    do
+    {
+        // Show the prompt
+        printf("$ ");
+        // Read and parse input from the user
+        read_line();
+        // Execute the command
+        run = execute_command();
+    } while (run);
+}
+
+void read_line()
+{
+}
+
+bool execute_command()
+{
+    return false;
 }
