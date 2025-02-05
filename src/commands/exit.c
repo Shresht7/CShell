148526b07a5a952c6ad98c5a22bae596c2ead681
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-05 12:32:10 +0530

    Enhance exit command to accept a status code argument

--- src/commands/exit.c
+++ src/commands/exit.c
@@ -6,9 +6,13 @@
 /// This function terminates the program by calling the `exit` function with
 /// `EXIT_SUCCESS` status. It does not use the `args` parameter.
 ///
-/// @param args Unused parameter.
+/// @param args The arguments passed in to the command. The first argument is the status code
 /// @return This function does not return.
 bool cmd_exit(char **args)
 {
+    if (args[1] != NULL)
+    {
+        exit(atoi(args[1]));
+    }
     exit(EXIT_SUCCESS);
 }
