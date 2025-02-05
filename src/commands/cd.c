bd1c3286dec404a7e770f214a3c953c543ac29b7
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-02-05 12:07:53 +0530

    Improve `cd` command

--- src/commands/cd.c
+++ src/commands/cd.c
@@ -1,5 +1,7 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #ifdef _WIN32
 #include <direct.h>
@@ -9,11 +11,45 @@
 #include <unistd.h>
 #endif
 
-bool cmd_cd(char **args)
+///  @brief Retrieves the home directory path of the current user.
+///
+///  This function fetches the home directory path from the environment variable "HOME".
+///  On Windows systems, if the "HOME" environment variable is not set, it attempts to
+///  retrieve the path from the "USERPROFILE" environment variable.
+///
+///  @return A pointer to a string containing the home directory path, or NULL if the
+///          environment variables are not set.
+char *get_home()
 {
-    if (args[1] == NULL)
+    char *home = getenv("HOME");
+#ifdef _WIN32
+    if (!home)
     {
-        fprintf(stderr, "CShell: expected argument\n");
+        // Copilot did this!
+        size_t len;
+        _dupenv_s(&home, &len, "USERPROFILE");
+    }
+#endif
+    return home;
+}
+
+bool cmd_cd(char **args)
+
+/// @brief Changes the current working directory.
+///
+/// If no argument is provided or the argument is "~", changes the directory to the user's home directory.
+/// If an argument is provided, changes the directory to the specified path.
+///
+/// @param args An array of arguments where args[1] is the directory to change to.
+/// @return true if the directory change is successful, otherwise prints an error message.
+{
+    char *home = get_home();
+    if (args[1] == NULL || strcmp(args[1], "~") == 0)
+    {
+        if (chdir(home) != 0)
+        {
+            perror("CShell");
+        }
     }
     else
     {
